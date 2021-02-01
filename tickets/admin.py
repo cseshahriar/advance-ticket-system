d67cc784353a84443882a36378fb8859f81838fe
cseshahriar <cse.shahriar.hosen@gmail.com>
2021-02-01 23:34:51 +0600

    color model admin and fixtores created

--- tickets/admin.py
+++ tickets/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from . models import (Priority,Category)
+from . models import (Priority,Category, Color)
 
 
 @admin.register(Priority)
@@ -12,6 +12,7 @@ class PriorityAdmin(admin.ModelAdmin):
     list_filter = ('created_at',)
     search_fields = ['name', 'code']
 
+
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     ordering = ['id']
@@ -21,3 +22,14 @@ class CategoryAdmin(admin.ModelAdmin):
     list_display_links = ('title',)
     list_filter = ('created_at',)
     search_fields = ('title',)
+
+
+@admin.register(Color)
+class ColorAdmin(admin.ModelAdmin):
+    ordering = ['id']
+    list_display = ('id', 'name', 'code', 'hex_code', 'is_active', 
+                    'created_at', 'updated_at')
+    list_display_links = ('name',)
+    list_filter = ('created_at',)
+    search_fields = ('name', 'code',)
+
