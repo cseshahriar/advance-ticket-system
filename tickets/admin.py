904a32757577c175898f958b2c5a0ee8ac376f4d
cseshahriar <cse.shahriar.hosen@gmail.com>
2021-02-01 22:29:55 +0600

    priority admin created

--- tickets/admin.py
+++ tickets/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from . models import (Priority,)
 
-# Register your models here.
+
+@admin.register(Priority)
+class PriorityAdmin(admin.ModelAdmin):
+    ordering = ['id']
+    list_display = (
+        'id', 'name', 'code', 'is_active', 'created_at', 'updated_at',)
+    list_display_links = ('name',)
+    list_filter = ('created_at',)
+    search_fields = ['name', 'code']
+
