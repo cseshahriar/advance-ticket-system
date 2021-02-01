402c7145395ffb5b9f0460798b20bb3d844e2a03
cseshahriar <cse.shahriar.hosen@gmail.com>
2021-02-01 22:07:11 +0600

    priority model created

--- tickets/models.py
+++ tickets/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Priority(models.Model):
+    name = models.CharField(max_length=30, unique=True)
+    code = models.CharField(max_length=30, unique=True)
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
