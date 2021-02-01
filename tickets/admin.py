bd05d07feb6317732081d435f999bf6881aab6f3
cseshahriar <cse.shahriar.hosen@gmail.com>
2021-02-01 22:58:15 +0600

    add verbose name

--- tickets/admin.py
+++ tickets/admin.py
@@ -1,13 +1,21 @@
 from django.contrib import admin
-from . models import (Priority,)
+from . models import (Priority,Category)
 
 
 @admin.register(Priority)
 class PriorityAdmin(admin.ModelAdmin):
     ordering = ['id']
     list_display = (
-        'id', 'name', 'code', 'is_active', 'created_at', 'updated_at',)
+        'id', 'name', 'code', 'is_active', 'created_at', 'updated_at',
+    )
     list_display_links = ('name',)
     list_filter = ('created_at',)
     search_fields = ['name', 'code']
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    ordering = ['id']
+    list_display = (
+        'id', 'title', 'slug', 'is_active', 'created_at', 'updated_at',
+    )
 
--- tickets/models.py
+++ tickets/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-
+from django.urls import reverse
+from django.utils.text import slugify
 
 class Priority(models.Model):
     name = models.CharField(max_length=30, unique=True)
@@ -10,3 +11,30 @@ class Priority(models.Model):
 
     def __str__(self):
         return self.name
+
+    class Meta:
+        verbose_name = 'priority'
+        verbose_name_plural = 'priorities'
+
+class Category(models.Model):
+    title = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(max_length=100, unique=True, null=True, 
+                                blank=True, editable=False)
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def get_absolute_url(self):
+        return reverse('ticket_detail', kwargs={'slug': self.slug})
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title, allow_unicode=True)
+        return super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
