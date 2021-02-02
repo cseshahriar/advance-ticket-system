a14435da95daa9affc918be12e2d92fb902ac412
cseshahriar <cse.shahriar.hosen@gmail.com>
2021-02-03 00:35:48 +0600

    tickets create template

--- ticket_system/urls.py
+++ ticket_system/urls.py
@@ -20,6 +20,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('tickets.urls')),
     path('ckeditor/', include('ckeditor_uploader.urls')),
 ]
 
--- tickets/views.py
+++ tickets/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView 
+from . models import Priority, Category, Color, Ticket, Attachment
 
-# Create your views here.
+class HomeView(CreateView):
+    model = Ticket
+    template_name = 'tickets/create.html'
+    form_class = None
+    formset_class = None
+    success_url = '/'
+
+    def get_context_data(self, **kwargs):
+        pass 
+
+    def form_valid(self, form):
+        pass 
