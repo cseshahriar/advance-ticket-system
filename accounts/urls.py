3cf5363d17c4caf0b2bb3d1f87ca8e4ad7d241ec
cseshahriar <cse.shahriar.hosen@gmail.com>
2021-02-07 23:01:29 +0600

    tickets list

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path 
 from django.contrib.auth import views as auth_views
-from . views import DashboardView
+from . views import (
+    DashboardView, TicketsListView
+)
 
 urlpatterns = [
     # user urls
@@ -17,6 +19,9 @@ urlpatterns = [
 
     # dashboard
     path('dashboard/', DashboardView.as_view(), name='accounts_dashboard'), 
+
+    # tickets urls
+    path('tickets/list', TicketsListView.as_view(), name='tickets_list'),
 ]
 
 """
--- accounts/views.py
+++ accounts/views.py
@@ -1,14 +1,16 @@
+import logging
 from django.shortcuts import render
 from django.views.generic.base import TemplateView
+from django.views.generic.list import ListView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.mixins import UserPassesTestMixin
 # from django.contrib.auth.mixins import PermissionRequiredMixin
+from tickets.models import Ticket
 
-import logging
 logger = logging.getLogger(__name__)
 
 class DashboardView(LoginRequiredMixin, UserPassesTestMixin, TemplateView):
-    
+    """ Dashboard Overview """
     login_url = '/accounts/login/'
     redirect_field_name = 'redirect_to'
     template_name = 'accounts/dashboard.html'
@@ -22,3 +24,21 @@ class DashboardView(LoginRequiredMixin, UserPassesTestMixin, TemplateView):
         # context['obj'] = model.objects.all() 
         logger.info('Dashboard Overview')
         return context
+
+
+class TicketsListView(LoginRequiredMixin, UserPassesTestMixin, ListView):
+    """ Tickets List View """
+
+    model = Ticket
+    paginate_by = 10
+    template_name = 'accounts/tickets/list.html'
+    login_url = '/accounts/login/'
+    redirect_field_name = 'redirect_to'
+
+    def test_func(self):
+        return self.request.user.is_staff or self.request.user.is_superuser
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['tickets'] = Ticket.objects.all().order_by('-pk')
+        return context 
